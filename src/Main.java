58b4fde6e0967596b03e10cd2f775db88c4623c2
elyagon <ehvelina-gg@yandex.ru>
2022-10-16 23:00:48 +0300

    hw14

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задание 1
+        System.out.println("Задание 1");
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
+        //Задание 2
+        System.out.println("Задание 2");
+        for (int a = 10; a > 0; a--) {
+            System.out.println(a);
+        }
+        //Задание 3
+        System.out.println("Задание 3");
+        for (int b = 0; b <= 17; b = b + 2)
+            System.out.println(b);
+        //Задание 4
+        System.out.println("Задание 4");
+        for (int c = 10; c > -11; c--) {
+            System.out.println(c);
+        }
+    }
+}
